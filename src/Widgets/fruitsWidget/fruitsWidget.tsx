9a1b669aa90efc46b5b78ebb480e493bc8b62f99
[Joel LIndberg] <[Joellindberg23@gmail.com]>
2025-02-19 15:09:03 +0100

    remove widget

---
+++ src/Widgets/fruitsWidget/fruitsWidget.tsx
@@ -0,0 +1,37 @@
+import  { useState } from "react";
+import Button from "../../Components/Button";
+import "./fruitsWidget.scss"; 
+
+const FruitsWidget = () => {
+  const [fruits, setFruits] = useState<string[]>([
+    "🍎 Apple",
+    "🍌 Banana",
+    "🍇 Grapes",
+    "🍊 Orange",
+    "🍉 Watermelon",
+  ]);
+
+  const removeFruit = (fruit: string) => {
+    setFruits(fruits.filter((f) => f !== fruit));
+  };
+
+  return (
+    <div className="fruits-widget">
+      <h2>Fruit List 🍏</h2>
+      {fruits.length > 0 ? (
+        <ul>
+          {fruits.map((fruit) => (
+            <li key={fruit} style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+              {fruit}
+              <Button text="Remove" onClick={() => removeFruit(fruit)} type="secondary" />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No more fruits left! 🫣</p>
+      )}
+    </div>
+  );
+};
+
+export default FruitsWidget;
